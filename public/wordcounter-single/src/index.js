ed978428dd93ec7ba4901cec584836a9f22408fa
David Woakes <david.woakes@barrachd.com>
2018-10-14 07:38:36 +0100

    Add SaveManager component to save state Add fake save state function

--- public/wordcounter-single/src/index.js
+++ public/wordcounter-single/src/index.js
@@ -45,6 +45,58 @@ function countWords(text) {
     return text ? text.match(/\w+/g).length : 0;
 }
 
+const SUCCESS = 'Success';
+const FAILURE = 'Failure';
+const WAITING = 'Waiting';
+const IDLE = 'Idle';
+
+function SaveButton({ onClick }) {
+    return (
+        <button className="pv2 ph3" onClick={onClick}>
+            Save
+        </button>
+    );
+}
+
+function AlertBox({ status }) {
+    if (status === FAILURE) {
+        return <div className="mv2">Save failed</div>;
+    } else if (status === SUCCESS) {
+        return <div className="mv2">Save successful</div>;
+    } else if (status === WAITING) {
+        return <div className="mv2">Saving...</div>;
+    } else {
+        return null;
+    }
+}
+
+class SaveManager extends React.Component {
+    constructor() {
+        super();
+        this.save = this.save.bind(this);
+        this.state = { saveStatus: IDLE };
+    }
+
+    render() {
+        return (
+            <div className="flex flex-column mv2">
+                <SaveButton onClick={this.save} />
+                <AlertBox status={this.state.saveStatus} />
+            </div>
+        )
+    }
+    save(event) {
+        event.preventDefault();
+        this.setState(() => ({ saveStatus: WAITING }));
+        this.props
+            .saveFunction(this.props.data)
+            .then(
+                success => this.setState(() => ({ saveStatus: SUCCESS })),
+                failure => this.setState(() => ({ saveStatus: FAILURE }))
+            );
+    }
+}
+
 class WordCounter extends React.Component {
     constructor() {
         super();
@@ -67,6 +119,7 @@ class WordCounter extends React.Component {
                     <Counter count={wordCount} />
                     <ProgressBar completion={progress} />
                 </div>
+                <SaveManager saveFunction={makeFakeRequest} data={this.state} />
             </form>
         );
     }
@@ -80,3 +133,16 @@ ReactDOM.render(
     <WordCounter targetWordCount={10} />,
     document.getElementById('app')
 );
+
+function makeFakeRequest() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (Math.random() > 0.5) {
+                resolve("Success");
+            }
+            else {
+                reject("Failure");
+            }
+        }, 500);
+    });
+}
