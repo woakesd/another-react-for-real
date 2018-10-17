9e1bdded80e2d0003953815f586a9d6c27203540
David Woakes <david.woakes@barrachd.com>
2018-10-17 13:41:54 +0100

    Added test for counting empty string

--- public/wordcounter/src/__tests__/count.test.js
+++ public/wordcounter/src/__tests__/count.test.js
@@ -1,5 +1,12 @@
 import countWords from '../countWords';
 
-it('counts the correct number of words', () => {
-    expect(countWords('one two three')).toBe(3);
-});
+describe('the counting function', () => {
+    it('counts the correct number of words', () => {
+        expect(countWords('one two three')).toBe(3);
+    });
+
+    it('counts an empty string', () => {
+        expect(countWords('')).toBe(0);
+    });
+})
+
