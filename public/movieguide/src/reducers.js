6edbbc41f73ae8cf75c9e971b4ef87307bd96147
David Woakes <david.woakes@barrachd.com>
2018-10-19 10:51:55 +0100

    Didn't save, so here is the first reducer

--- public/movieguide/src/reducers.js
+++ public/movieguide/src/reducers.js
@@ -1,1 +1,10 @@
-import { FILTERS_CHANGED, MOVIES_LOADED, FAVORITED, UNFAVORITED } from 'actions.js';
+import { FILTERS_CHANGED, MOVIES_LOADED, FAVORITED, UNFAVORITED } from './actions';
+
+export function movies(state = [], action) {
+    switch (action.type) {
+        case MOVIES_LOADED:
+            return action.movies;
+        default:
+            return state;
+    }
+}
