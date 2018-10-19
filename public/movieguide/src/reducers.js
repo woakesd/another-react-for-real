a98bca58458f9a675232382c5d35559dc01d954f
David Woakes <david.woakes@barrachd.com>
2018-10-19 11:20:11 +0100

    Add reducers

--- public/movieguide/src/reducers.js
+++ public/movieguide/src/reducers.js
@@ -1,9 +1,29 @@
-import { FILTERS_CHANGED, MOVIES_LOADED, FAVORITED, UNFAVORITED } from './actions';
+import { FILTER_CHANGED, MOVIES_LOADED, FAVORITED, UNFAVORITED } from './actions';
 
 export function movies(state = [], action) {
     switch (action.type) {
         case MOVIES_LOADED:
             return action.movies;
+        default:
+            return state;
+    }
+}
+
+export function filter(state = false, action) {
+    switch (action.type) {
+        case FILTER_CHANGED:
+            return action.filter;
+        default:
+            return state;
+    }
+}
+
+export function favorites(state = [], action) {
+    switch (action.type) {
+        case FAVORITED:
+            return [...state, action.movieId];
+        case UNFAVORITED:
+            return state.filter(id => id !== action.movieId);
         default:
             return state;
     }
