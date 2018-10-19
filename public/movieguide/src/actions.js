6935ef21c128279edcd3372339e6049cb98c82ee
David Woakes <david.woakes@barrachd.com>
2018-10-19 11:19:42 +0100

    Change action to filter changed

--- public/movieguide/src/actions.js
+++ public/movieguide/src/actions.js
@@ -1,4 +1,4 @@
-export const FILTERS_CHANGED = 'FILTERS_CHANGED';
+export const FILTER_CHANGED = 'FILTERS_CHANGED';
 export const MOVIES_LOADED = 'MOVIES_LOADED';
 export const FAVORITED = 'FAVORITED';
 export const UNFAVORITED = 'UNFAVORITED';
